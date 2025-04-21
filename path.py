247840905b673a9ba2452c2fbd3010b7596a0af8
Alexey Po <flexxus@mail.ru>
2025-04-21 18:05:37 +0300

    add files README.md, path.py, requirements.txt, text1.txt

---
+++ path.py
@@ -0,0 +1,9 @@
+import os
+
+# current_directory = os.getcwd()
+# print(current_directory)
+
+# print(os.path.dirname(__file__))
+# print(os.path.abspath(__file__))
+
+PATH_TO_TEXT = os.path.join(os.path.dirname(__file__), 'texts', 'text1.txt')
